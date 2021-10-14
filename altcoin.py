5887c8133c3288ee02bccb8e5a5b63ac5ef01a42
bboodd <92453279+bboodd@users.noreply.github.com>
2021-10-14 12:25:54 +0900

    Update altcoin.py

--- altcoin.py
+++ altcoin.py
@@ -5,12 +5,12 @@ access = "rnbsbUeuw96s99VAt6WOEit4cxY39c68cECyxr4Q"
 secret = "XpQqKIsfOD1yOvofIYnQnq9nWBEALH0ZmEUMx1DX"
 def get_target_price(ticker, k):
     """변동성 돌파 전략으로 매수 목표가 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
+    df = pyupbit.get_ohlcv(ticker, interval="minute60", count=2)
     target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k
     return target_price
 def get_start_time(ticker):
     """시작 시간 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="day", count=1)
+    df = pyupbit.get_ohlcv(ticker, interval="minute60", count=1)
     start_time = df.index[0]
     return start_time
 def get_balance(ticker):
@@ -35,7 +35,7 @@ while True:
     try:
         now = datetime.datetime.now()
         start_time = get_start_time("KRW-BTC")
-        end_time = start_time + datetime.timedelta(days=1)
+        end_time = start_time + datetime.timedelta(minutes=60)
         if start_time < now < end_time - datetime.timedelta(seconds=10):
             target_price = get_target_price("KRW-STPT", 0.5)
             current_price = pyupbit.get_current_price("KRW-STPT")
