cc970bd9e590c726d2713d5a052578875406794a
bboodd <92453279+bboodd@users.noreply.github.com>
2021-10-14 20:48:04 +0900

    Update altcoin.py

--- altcoin.py
+++ altcoin.py
@@ -3,14 +3,18 @@ import pyupbit
 import datetime
 access = "rnbsbUeuw96s99VAt6WOEit4cxY39c68cECyxr4Q"
 secret = "XpQqKIsfOD1yOvofIYnQnq9nWBEALH0ZmEUMx1DX"
+coinlist = ["KRW-HUM", "KRW-STPT", "KRW-DOT", "KRW-QKC", "KRW-MLK", "KRW-OMG", "KRW-IQ",
+"KRW-STX", "KRW-CBK", "KRW-ARK", "KRW-WAVES", "KRW-VET", "KRW-XLM", "KRW-ETC", "KRW-MTL"]
+
+
 def get_target_price(ticker, k):
     """변동성 돌파 전략으로 매수 목표가 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="minute60", count=2)
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
     target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k
     return target_price
 def get_start_time(ticker):
     """시작 시간 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="minute60", count=1)
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=1)
     start_time = df.index[0]
     return start_time
 def get_balance(ticker):
@@ -31,145 +35,26 @@ def get_balance(ticker):
 upbit = pyupbit.Upbit(access, secret)
 print("autotrade start")
 # 자동매매 시작
-while True:
-    try:
-        now = datetime.datetime.now()
-        start_time = get_start_time("KRW-BTC")
-        end_time = start_time + datetime.timedelta(minutes=60)
-        if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-STPT", 0.5)
-            current_price = pyupbit.get_current_price("KRW-STPT")
-            if target_price < current_price:
-                krw = get_balance("KRW")
-                if krw > 5000:
-                    upbit.buy_market_order("KRW-STPT", krw*0.9995)
-        elif start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-HUM", 0.5)
-            current_price = pyupbit.get_current_price("KRW-HUM")
-            if target_price < current_price:
-                krw = get_balance("KRW")
-                if krw > 5000:
-                    upbit.buy_market_order("KRW-HUM", krw*0.9995)
-        elif start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-STX", 0.5)
-            current_price = pyupbit.get_current_price("KRW-STX")
-            if target_price < current_price:
-                krw = get_balance("KRW")
-                if krw > 5000:
-                    upbit.buy_market_order("KRW-STX", krw*0.9995)
-        elif start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-OMG", 0.5)
-            current_price = pyupbit.get_current_price("KRW-OMG")
-            if target_price < current_price:
-                krw = get_balance("KRW")
-                if krw > 5000:
-                    upbit.buy_market_order("KRW-OMG", krw*0.9995)
-        elif start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-QKC", 0.5)
-            current_price = pyupbit.get_current_price("KRW-QKC")
-            if target_price < current_price:
-                krw = get_balance("KRW")
-                if krw > 5000:
-                    upbit.buy_market_order("KRW-QKC", krw*0.9995)
-        elif start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-XEC", 0.5)
-            current_price = pyupbit.get_current_price("KRW-XEC")
-            if target_price < current_price:
-                krw = get_balance("KRW")
-                if krw > 5000:
-                    upbit.buy_market_order("KRW-XEC", krw*0.9995)
-        elif start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-HUNT", 0.5)
-            current_price = pyupbit.get_current_price("KRW-HUNT")
-            if target_price < current_price:
-                krw = get_balance("KRW")
-                if krw > 5000:
-                    upbit.buy_market_order("KRW-HUNT", krw*0.9995)
-        elif start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-AHT", 0.5)
-            current_price = pyupbit.get_current_price("KRW-AHT")
-            if target_price < current_price:
-                krw = get_balance("KRW")
-                if krw > 5000:
-                    upbit.buy_market_order("KRW-AHT", krw*0.9995)
-        elif start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-AXS", 0.5)
-            current_price = pyupbit.get_current_price("KRW-AXS")
-            if target_price < current_price:
-                krw = get_balance("KRW")
-                if krw > 5000:
-                    upbit.buy_market_order("KRW-AXS", krw*0.9995)
-        elif start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-ARK", 0.5)
-            current_price = pyupbit.get_current_price("KRW-ARK")
-            if target_price < current_price:
-                krw = get_balance("KRW")
-                if krw > 5000:
-                    upbit.buy_market_order("KRW-ARK", krw*0.9995)
-        elif start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-TON", 0.5)
-            current_price = pyupbit.get_current_price("KRW-TON")
-            if target_price < current_price:
-                krw = get_balance("KRW")
-                if krw > 5000:
-                    upbit.buy_market_order("KRW-TON", krw*0.9995)
-        elif start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-CBK", 0.5)
-            current_price = pyupbit.get_current_price("KRW-CBK")
-            if target_price < current_price:
-                krw = get_balance("KRW")
-                if krw > 5000:
-                    upbit.buy_market_order("KRW-CBK", krw*0.9995)
-        elif start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-WAVES", 0.5)
-            current_price = pyupbit.get_current_price("KRW-WAVES")
-            if target_price < current_price:
-                krw = get_balance("KRW")
-                if krw > 5000:
-                    upbit.buy_market_order("KRW-WAVES", krw*0.9995)
-        else:
-            stpt = get_balance("STPT")
-            hum = get_balance("HUM")
-            stx = get_balance("STX")
-            omg = get_balance("OMG")
-            qkc = get_balance("QKC")
-            xec = get_balance("XEC")
-            hunt = get_balance("HUNT")
-            aht = get_balance("AHT")
-            axs = get_balance("AXS")
-            ark = get_balance("ARK")
-            ton = get_balance("TON")
-            cbk = get_balance("CBK")
-            waves = get_balance("WAVES")
 
-            if stpt > 0.00008:
-                upbit.sell_market_order("KRW-STPT", stpt*0.9995)
-            elif hum > 0.00008:
-                upbit.sell_market_order("KRW-HUM", hum*0.9995)
-            elif stx > 0.00008:
-                upbit.sell_market_order("KRW-STX", stx*0.9995)
-            elif omg > 0.00008:
-                upbit.sell_market_order("KRW-OMG", omg*0.9995)
-            elif qkc > 0.00008:
-                upbit.sell_market_order("KRW-QKC", qkc*0.9995)
-            elif xec > 0.00008:
-                upbit.sell_market_order("KRW-XEC", xec*0.9995)
-            elif hunt > 0.00008:
-                upbit.sell_market_order("KRW-HUNT", hunt*0.9995)
-            elif aht > 0.00008:
-                upbit.sell_market_order("KRW-AHT", aht*0.9995)
-            elif axs > 0.00008:
-                upbit.sell_market_order("KRW-AXS", axs*0.9995)
-            elif ark > 0.00008:
-                upbit.sell_market_order("KRW-ARK", ark*0.9995)
-            elif ton > 0.00008:
-                upbit.sell_market_order("KRW-TON", ton*0.9995)
-            elif cbk > 0.00008:
-                upbit.sell_market_order("KRW-CBK", cbk*0.9995)
-            elif waves > 0.00008:
-                upbit.sell_market_order("KRW-WAVES", waves*0.9995)
-        time.sleep(1)
-    except Exception as e:
-        print(e)
-        time.sleep(1)
-        
+
+while True: 
+    for i in coinlist:
+        try: 
+            now = datetime.datetime.now()
+            start_time = get_start_time("KRW-BTC")
+            end_time = start_time + datetime.timedelta(days=1)
+            if start_time < now < end_time - datetime.timedelta(seconds=10):
+                target_price = get_target_price(i, 0.5)
+                current_price = pyupbit.get_current_price(i)
+                if target_price < current_price:
+                    krw = get_balance("KRW")
+                    if krw > 5000:
+                        upbit.buy_market_order(i, krw*0.9995)
+            else:
+                rc = get_balance(i)
+                if rc > 0.00008:
+                    upbit.sell_market_order(i, i*0.9995)
+            time.sleep(1)
+        except Exception as e:
+            print(e)
+            time.sleep(1)
